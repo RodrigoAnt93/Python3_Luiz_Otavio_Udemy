07a7f99155b5ebe34058a6ed6f0ccb9ef4c8864c
RodrigoAnt93 <rodrigoscfc@gmail.com>
2023-04-24 21:59:02 -0300

    Delete Exerc_57.py

--- Exerc_57.py
+++
@@ -1,4 +0,0 @@
-altura = int(input('Quantos cm de altura? '))
-comprimento = int(input('Quantos cm de comprimento? '))
-
-print(f'Com um painel de {altura}x{comprimento}cm serão usados {(altura+1) * (comprimento+1)}')
