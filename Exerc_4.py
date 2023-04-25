5bc52f37f4206f8d7a993d82ba7e0cfcc301f3ed
RodrigoAnt93 <rodrigoscfc@gmail.com>
2023-04-24 21:57:19 -0300

    Delete Exerc_4.py

--- Exerc_4.py
+++
@@ -1,7 +0,0 @@
-nome = str(input('Digite seu nome: '))
-if len(nome) <= 4:
-    print('Seu nome é pequeno...')
-elif 5 <= len(nome) <= 6:
-    print('Seu nome é bem normal.')
-else:
-    print('Poxa! Seu nome é muito grande...')
