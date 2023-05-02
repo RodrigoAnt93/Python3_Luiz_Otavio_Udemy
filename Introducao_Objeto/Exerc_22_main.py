dbd4f22cd170ff8a2f3da8c3d442c37bdfeb1489
RodrigoAnt93 <rodrigoscfc@gmail.com>
2023-05-02 13:21:37 -0300

    Create Exerc_22_main.py

---
+++ Introducao_Objeto/Exerc_22_main.py
@@ -0,0 +1,5 @@
+#Crie uma classe abstrata chamada "Pokemon" com atributos como nome, tipo, saúde, ataque, defesa e velocidade.
+#Implemente um método abstrato chamado "atacar" que recebe outro objeto "Pokemon" e calcula o dano com
+#base na diferença entre o ataque do atacante e a defesa do defensor.
+# Crie pelo menos duas subclasses concretas da classe "Pokemon" com valores diferentes para os atributos.
+
