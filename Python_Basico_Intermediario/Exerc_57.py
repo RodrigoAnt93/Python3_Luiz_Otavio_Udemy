beea6e9619e47ebf4171d0a5432480f77c4746b6
RodrigoAnt93 <rodrigoscfc@gmail.com>
2023-05-02 13:22:12 -0300

    Create Exerc_57.py

---
+++ Python_Basico_Intermediario/Exerc_57.py
@@ -0,0 +1,4 @@
+altura = int(input('Quantos cm de altura? '))
+comprimento = int(input('Quantos cm de comprimento? '))
+
+print(f'Com um painel de {altura}x{comprimento}cm serão usados {(altura+1) * (comprimento+1)}')
