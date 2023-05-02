f7626057d55b7d93b3e6f3021b43c344d5291997
RodrigoAnt93 <rodrigoscfc@gmail.com>
2023-05-02 13:21:27 -0300

    Update main.py

--- Introducao_Objeto/Exerc_20/main.py
+++ Introducao_Objeto/Exerc_20/main.py
@@ -1,24 +1,7 @@
-class Log:
-    def _log(self, msg):
-        raise NotImplementedError('Implemente o método log')
+from eletronico import Smartphone
 
-    def log_error(self, msg):
-        self._log(f'Error: {msg}')
+galaxy_s = Smartphone('Galaxy S')
+iphone = Smartphone('iPhone')
 
-    def log_success(self, msg):
-        self._log(f'Success: {msg}')
-
-class LogFileMixin(Log):
-    def _log(self, msg):
-        print(msg, 'FileMixin!!!!')
-
-class LogPrintMixin(Log):
-    def _log(self, msg):
-        print(msg, 'PrintMixin')
-
-
-if __name__ == '__main__':
-    lf = LogFileMixin()
-    lf.log_error('ddd')
-    lp = LogPrintMixin()
-    lp.log_error('qualquer coisa')
+galaxy_s.ligar()
+iphone.desligar()
