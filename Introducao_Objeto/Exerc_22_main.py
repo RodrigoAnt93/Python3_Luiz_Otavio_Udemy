e005f13b3ba13933b81abb9f2670ef71434d9671
RodrigoAnt93 <rodrigoscfc@gmail.com>
2023-05-02 21:27:58 -0300

    Update Exerc_22_main.py

--- Introducao_Objeto/Exerc_22_main.py
+++ Introducao_Objeto/Exerc_22_main.py
@@ -1,5 +1,10 @@
-#Crie uma classe abstrata chamada "Pokemon" com atributos como nome, tipo, saúde, ataque, defesa e velocidade.
-#Implemente um método abstrato chamado "atacar" que recebe outro objeto "Pokemon" e calcula o dano com
-#base na diferença entre o ataque do atacante e a defesa do defensor.
-# Crie pelo menos duas subclasses concretas da classe "Pokemon" com valores diferentes para os atributos.
+pokemon = [{'name': 'Bulbasaur', "atbt_base": [45, 50, 50, 65, 65, 45], 'type': ['Planta', 'Venenoso'], 'move': {1: ['Tackle', 35], 2: ['Vine Whip', 35]}},
+           {"name": "Caterpie", "atbt_base": [45, 30, 35, 20, 20, 20], "type": ["Inseto"], 'move': {1: ['Tackle', 35]}}]
+from Exerc_22_class import Pokemon, Battler
 
+bulba = Pokemon(**pokemon[0], lvl=5)
+caterpie = Pokemon(**pokemon[1], lvl=10)
+
+current_battle = Battler(bulba, caterpie)
+
+current_battle.battle()
