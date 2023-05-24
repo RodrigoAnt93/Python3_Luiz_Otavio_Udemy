b13d9509a563ac8ecde1aea6cb164591397ba5e2
RodrigoAnt93 <rodrigoscfc@gmail.com>
2023-05-23 23:02:22 -0300

    Create Exerc_35_func.py

---
+++ Introducao_Objeto/Exerc_35_func.py
@@ -0,0 +1,30 @@
+from Exerc_35_class import AccountBank
+
+
+def deposit(obj: AccountBank, value: float):
+    obj.deposit(value)
+    return print(f"R${value} foi depositado na conta.")
+
+
+def draw(obj: AccountBank, value: float):
+    result = obj.draw(value)
+    if result == 1:
+        return print(f"Saque Autorizado. Valor de R${value} será sacado.")
+    else:
+        print("Saque não autorizado.")
+
+def activate_account(obj: AccountBank):
+    result = obj.activate_account()
+    if result == 1:
+        print("A conta foi ativada.")
+    else:
+        print("Conta já ativa.")
+
+
+def situation_account(obj: AccountBank):
+    result = obj.situation_account()
+    print(f'    O status da conta é {result[0]}')
+    print(f'    O saldo é R${result[1]}')
+    if result[2]:
+        print(f"    Seu cheque especial é R${result[3]}")
+        print(f"    Seu saldo do cheque especial é R${result[4]}")
