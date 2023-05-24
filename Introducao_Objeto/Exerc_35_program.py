91903b85650b34df551c439fb2f8a6d137f7f11e
RodrigoAnt93 <rodrigoscfc@gmail.com>
2023-05-23 23:02:29 -0300

    Create Exerc_35_program.py

---
+++ Introducao_Objeto/Exerc_35_program.py
@@ -0,0 +1,36 @@
+from Exerc_35_class import AccountBank
+import Exerc_35_func
+
+client = AccountBank(1234, 100, "Rodrigo", 1, False, [True, 100, 100])
+
+while True:
+    print("=-=" * 5, " Banco CDD ", "=-=" * 5)
+    print("O que você deseja: \n"
+          "1 - Depositar\n"
+          "2 - Sacar:\n"
+          "3 - Ativar conta\n"
+          "4 - Situação da conta\n"
+          "5 - Sair")
+    opc = int(input('Digite uma opção: '))
+    if opc == 1:
+        value = float(input("   Digite o valor a ser depositado: R$"))
+        print("   ", end="")
+        Exerc_35_func.deposit(client, value)
+    elif opc == 2:
+        value = float(input("   Digite o valor a ser sacado: R$"))
+        print("   ", end="")
+        Exerc_35_func.draw(client, value)
+    elif opc == 3:
+        opc = input("   Você deseja ativar sua conta? [S/N] ").strip().upper()[0]
+        if opc == 'S':
+            print("   ", end="")
+            Exerc_35_func.activate_account(client)
+        else:
+            print("Opção cancelada.")
+    elif opc == 4:
+        Exerc_35_func.situation_account(client)
+
+    else:
+        break
+    print('')
+print("Fim do Programa.")
