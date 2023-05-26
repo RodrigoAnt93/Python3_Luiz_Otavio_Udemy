7e5aec89734a62cd19a0f8c9e63d0f73f94cf58e
Rodrigo Antonio <119074329+RodrigoAnt93@users.noreply.github.com>
2023-05-26 15:19:40 -0300

    Update Exerc_35_program.py

--- Introducao_Objeto/Exerc_35_program.py
+++ Introducao_Objeto/Exerc_35_program.py
@@ -1,62 +1,39 @@
-class AccountBank:
-    def __init__(self, number: int, name: str, type: int):
-        self.number = number
-        self.balance = 0
-        self.name = name
-        self.type = type
-        self.status = False
-        self.limit = [False, 0, 0] #[Status, value overdraft, balance overdraft]
+from Exerc_35_class import AccountBank
+import Exerc_35_func
 
-    def limit_(self, values) -> int:
-        if not self.limit[0]:
-            self.limit[0] = True
-            self.limit[1] = values
-            self.limit[2] = values
-            return 1
+client = AccountBank(1234, "Rodrigo", 1)
+
+while True:
+    print("=-=" * 5, " Banco CDD ", "=-=" * 5)
+    print("O que você deseja: \n"
+          "1 - Depositar\n"
+          "2 - Sacar:\n"
+          "3 - Ativar conta\n"
+          "4 - Situação da conta\n"
+          "5 - Ativar cheque especial\n"
+          "6 - Sair")
+    opc = int(input('Digite uma opção: '))
+    if opc == 1:
+        value = float(input("   Digite o valor a ser depositado: R$"))
+        print("   ", end="")
+        Exerc_35_func.deposit(client, value)
+    elif opc == 2:
+        value = float(input("   Digite o valor a ser sacado: R$"))
+        print("   ", end="")
+        Exerc_35_func.draw(client, value)
+    elif opc == 3:
+        opc = input("   Você deseja ativar sua conta? [S/N] ").strip().upper()[0]
+        if opc == 'S':
+            print("   ", end="")
+            Exerc_35_func.activate_account(client)
         else:
-            return 0
-
-    def deposit(self, values: float) -> None:
-        if self.limit[0]:
-            if self.limit[2] < self.limit[1]:
-                difference = self.limit[1] - self.limit[2]
-                if values > difference:
-                    self.limit[2] += difference
-                    values = values - difference
-                    self.balance += values
-                else:
-                    self.limit[2] += values
-            else:
-                self.balance += values
-        else:
-            self.balance += values
-
-    def draw(self, values: float) -> int:
-        if self.status:
-            if values > self.balance:
-                difference = values - self.balance
-                if self.limit[0]:
-                    if difference < self.limit[2]:
-                        self.balance = 0
-                        self.limit[2] -= difference
-                        return 1
-                    else:
-                        return 0
-                else:
-                    return 0
-            else:
-                self.balance -= values
-                return 1
-        else:
-            return 0
-
-    def activate_account(self) -> int:
-        if self.status:
-            return 0
-        else:
-            self.status = True
-            return 1
-
-    def situation_account(self) -> tuple:
-        return self.status, self.balance, self.limit[0], self.limit[1], self.limit[2]
-
+            print("Opção cancelada.")
+    elif opc == 4:
+        Exerc_35_func.situation_account(client)
+    elif opc == 5:
+        value = float(input("   Qual o valor do crédito do cheque especial? "))
+        Exerc_35_func.activa_limit(client, value)
+    else:
+        break
+    print('')
+print("Fim do Programa.")
